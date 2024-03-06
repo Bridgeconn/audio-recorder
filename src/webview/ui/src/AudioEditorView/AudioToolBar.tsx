0956612278b285815111f02773abb08bd37537f9
vipinpaul <vipin.paul@bridgeconn.com>
2024-03-06 17:18:14 +0530

    Configured the audio player buttons

--- src/webview/ui/src/AudioEditorView/AudioToolBar.tsx
+++ src/webview/ui/src/AudioEditorView/AudioToolBar.tsx
@@ -1,66 +1,72 @@
-import { useState } from "react";
-import { IAudioData } from "../../../../types/editor";
-import Delete from "../IconsComponents/Delete";
-import Pause from "../IconsComponents/Pause";
-import Play from "../IconsComponents/Play";
-import Record from "../IconsComponents/Record";
-import Rewind from "../IconsComponents/Rewind";
-import Stop from "../IconsComponents/Stop";
-import Waveform from "./Waveform";
+import { useState } from 'react';
+import { IAudioData } from '../../../../types/editor';
+import Delete from '../IconsComponents/Delete';
+import Pause from '../IconsComponents/Pause';
+import Play from '../IconsComponents/Play';
+import Record from '../IconsComponents/Record';
+import Rewind from '../IconsComponents/Rewind';
+import Stop from '../IconsComponents/Stop';
+import Waveform from './Waveform';
 
 function AudioToolBar({ audioData }: { audioData: IAudioData | undefined }) {
-  const [playPause, setPlayPause] = useState(false);
+	const [playPause, setPlayPause] = useState(false);
+	const [control, setControl] = useState('');
 
-  const handlePlayAudio = () => {
-    setPlayPause((prev) => !prev);
-  };
+	// const handlePlayAudio = () => {
+	// 	setPlayPause((prev) => !prev);
+	// };
 
-  return (
-    <div
-      className="w-[99%] h-7 border border-gray-600 my-1 px-2 py-1 -bottom-10 right-0 self-center
-        bg-[var(--vscode-textSeparator-foreground)] flex gap-5 items-center"
-    >
-      {/* Waves */}
-      <div className="flex-1">
-        {audioData?.default && (
-          <Waveform
-            url={audioData[audioData["default"]]}
-            playPause={playPause}
-          />
-        )}
-      </div>
+	return (
+		<div
+			className='w-[99%] h-7 border border-gray-600 my-1 px-2 py-1 -bottom-10 right-0 self-center
+        bg-[var(--vscode-textSeparator-foreground)] flex gap-5 items-center'>
+			{/* Waves */}
+			<div className='flex-1'>
+				{audioData?.default && (
+					<Waveform
+						url={audioData[audioData['default']]}
+						playPause={playPause}
+						control={control}
+					/>
+				)}
+			</div>
 
-      {/* Buttons */}
-      <div className="flex gap-2 items-center">
-        <button className="cursor-pointer flex justify-center items-center">
-          <Record classes="w-5 h-5 stroke-red-500 hover:stroke-red-700" />
-        </button>
+			{/* Buttons */}
+			<div className='flex gap-2 items-center'>
+				<button className='cursor-pointer flex justify-center items-center'>
+					<Record classes='w-5 h-5 stroke-red-500 hover:stroke-red-700' />
+				</button>
 
-        <button className="cursor-pointer flex justify-center items-center">
-          <Stop classes="w-5 h-5  stroke-red-500 hover:stroke-red-700" />
-        </button>
+				<button className='cursor-pointer flex justify-center items-center'>
+					<Stop classes='w-5 h-5  stroke-red-500 hover:stroke-red-700' />
+				</button>
 
-        <button
-          className="cursor-pointer flex justify-center items-center"
-          onClick={handlePlayAudio}
-        >
-          <Play classes="w-5 h-5  stroke-green-500 hover:stroke-green-700" />
-        </button>
+				{control === 'play' ? (
+					<button
+						className='cursor-pointer flex justify-center items-center'
+						onClick={() => setControl('pause')}>
+						<Pause classes='w-6 h-6 fill-blue-500 hover:fill-blue-600' />
+					</button>
+				) : (
+					<button
+						className='cursor-pointer flex justify-center items-center'
+						onClick={() => setControl('play')}>
+						<Play classes='w-5 h-5  stroke-green-500 hover:stroke-green-700' />
+					</button>
+				)}
 
-        <button className="cursor-pointer flex justify-center items-center">
-          <Pause classes="w-6 h-6 fill-blue-500 hover:fill-blue-600" />
-        </button>
+				<button
+					className='cursor-pointer flex justify-center items-center'
+					onClick={() => setControl('rewind')}>
+					<Rewind classes='w-5 h-5 stroke-red-500 hover:stroke-red-600' />
+				</button>
 
-        <button className="cursor-pointer flex justify-center items-center">
-          <Delete classes="w-5 h-5 stroke-red-500 hover:stroke-red-600" />
-        </button>
-
-        <button className="cursor-pointer flex justify-center items-center">
-          <Rewind classes="w-5 h-5 stroke-red-500 hover:stroke-red-600" />
-        </button>
-      </div>
-    </div>
-  );
+				<button className='cursor-pointer flex justify-center items-center'>
+					<Delete classes='w-5 h-5 stroke-red-500 hover:stroke-red-600' />
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default AudioToolBar;
--- src/webview/ui/src/AudioEditorView/Waveform.tsx
+++ src/webview/ui/src/AudioEditorView/Waveform.tsx
@@ -1,84 +1,107 @@
-import React, {
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import WaveSurfer from "wavesurfer.js";
+import { useCallback, useEffect, useRef, useState } from 'react';
+import WaveSurfer from 'wavesurfer.js';
 
 interface IWaveformProps {
-  url: string;
-  playPause: boolean;
+	url: string;
+	playPause: boolean;
+	control: string;
 }
 
-function Waveform({ url, playPause }: IWaveformProps) {
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const wavesurfer = useRef<WaveSurfer | null>(null);
+function Waveform({ url, playPause, control }: IWaveformProps) {
+	const containerRef = useRef<HTMLDivElement | null>(null);
+	const wavesurfer = useRef<WaveSurfer | null>(null);
 
-  // state holds audio data - later use if needed
-  const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
-  const [audioUrl, setAudioUrl] = useState<string>("");
+	// state holds audio data - later use if needed
+	const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
+	const [audioUrl, setAudioUrl] = useState<string>('');
 
-  /**
-   * fetch audio data and load wave
-   */
-  function fetchAudioFile(audioPath:string) {
-    fetch(audioPath)
-      .then((response) => response.arrayBuffer())
-      .then((arrayBuffer) => {
-        console.log({ arrayBuffer });
-        // const blob = new Blob([arrayBuffer], { type: "audio/mpeg" });
-        const blob = new Blob([arrayBuffer]);
-        const url = URL.createObjectURL(blob);
-        setAudioBlob(blob)
-        setAudioUrl(url);
-        // const audio = new Audio(url)
-        // audio.play()
+	/**
+	 * fetch audio data and load wave
+	 */
+	function fetchAudioFile(audioPath: string) {
+		fetch(audioPath)
+			.then((response) => response.arrayBuffer())
+			.then((arrayBuffer) => {
+				console.log({ arrayBuffer });
+				// const blob = new Blob([arrayBuffer], { type: "audio/mpeg" });
+				const blob = new Blob([arrayBuffer]);
+				const url = URL.createObjectURL(blob);
+				setAudioBlob(blob);
+				setAudioUrl(url);
+				// const audio = new Audio(url)
+				// audio.play()
 
-        wavesurfer.current = WaveSurfer.create({
-          container: "#wav-container",
-          waveColor: "rgb(200, 0, 200)",
-          progressColor: "#0073E5",
-          cursorColor: "OrangeRed",
-          height: 100,
-          hideScrollbar: true,
-          interact: true,
-          backend: "MediaElement",
-          // plugins: [
-          //   microphone,
-          // ],
-        });
+				wavesurfer.current = WaveSurfer.create({
+					container: '#wav-container',
+					waveColor: 'rgb(200, 0, 200)',
+					progressColor: '#0073E5',
+					cursorColor: 'OrangeRed',
+					height: 'auto',
+					hideScrollbar: true,
+					interact: true,
+					backend: 'MediaElement',
+					// plugins: [
+					//   microphone,
+					// ],
+				});
 
-        // wavesurfer.current?.load("https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3");
-        //vscode :  https://file%2B.vscode-resource.vscode-cdn.net/home/siju/Music/Empty%20-%20new%20audio/audio/ingredients/GEN/1/1_1_1_default.mp3
-        wavesurfer.current?.load(url);
-      });
-  }
-  
-  // initial load audio
-  useEffect(() => {
-    fetchAudioFile(url);
-  }, [url]);
+				// wavesurfer.current?.load("https://www.mfiles.co.uk/mp3-downloads/brahms-st-anthony-chorale-theme-two-pianos.mp3");
+				//vscode :  https://file%2B.vscode-resource.vscode-cdn.net/home/siju/Music/Empty%20-%20new%20audio/audio/ingredients/GEN/1/1_1_1_default.mp3
+				wavesurfer.current?.load(url);
+			});
+	}
 
-  const onPlayPause = useCallback(() => {
-    console.log("click play pause ======");
+	// initial load audio
+	useEffect(() => {
+		fetchAudioFile(url);
+	}, [url]);
 
-    wavesurfer.current && wavesurfer.current.playPause();
-  }, [wavesurfer]);
+	const onPlay = useCallback(() => {
+		console.log('click play ======');
 
-  useEffect(() => {
-    onPlayPause();
-  }, [playPause]);
+		wavesurfer.current && wavesurfer.current.play();
+	}, [wavesurfer]);
 
-  return (
-    <>
-      <div
-        className="w-full"
-        ref={containerRef}
-        id="wav-container"
-      />
-    </>
-  );
+	const onPause = useCallback(() => {
+		console.log('click pause ======');
+
+		wavesurfer.current && wavesurfer.current.pause();
+	}, [wavesurfer]);
+
+	const onRewind = useCallback(() => {
+		console.log('click Rewind ======');
+
+		wavesurfer.current && wavesurfer.current.stop();
+	}, [wavesurfer]);
+
+	useEffect(() => {
+		switch (control) {
+			case 'play': {
+				onPlay();
+				break;
+			}
+			case 'pause': {
+				onPause();
+				break;
+			}
+			case 'rewind': {
+				onRewind();
+				break;
+			}
+			default:
+				break;
+		}
+	}, [control]);
+
+	return (
+		<>
+			<div
+				className='w-full relative h-6'
+				ref={containerRef}
+				id='wav-container'
+			/>
+		</>
+	);
 }
 
 export default Waveform;
