f5c5f0450cac8345a7098968dc4e544bcd2be2ce
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-03-07 17:07:40 +0530

    added Recording indicator in ui

--- src/webview/ui/tailwind.config.js
+++ src/webview/ui/tailwind.config.js
@@ -2,10 +2,14 @@
 // const colors = require('tailwindcss/colors');
 
 export default {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: "ping 2s linear infinite",
+      keyframes: {
+        ping: { "75%, 100%": { transform: "scale(1.2)", opacity: 0 } },
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
