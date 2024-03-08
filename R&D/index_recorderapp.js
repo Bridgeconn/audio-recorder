29940a92c1c9dd9b3e393ab3fba3db6dd080003a
svishnu06 <svishnu06@gmail.com>
2024-03-08 16:57:58 +0530

    files of conversion and recorderapp added

---
+++ R&D/index_recorderapp.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+const app = express();
+const PORT = 4001;
+
+let recordingProcess;
+let outputFile = 'output.wav';
+let outputFileIndex = 1;
+
+app.use(express.static('public'));
+
+app.get('/start', (req, res) => {
+  if (!recordingProcess) {
+    // Check if the output file exists
+    while (fs.existsSync(outputFile)) {
+      outputFileIndex++;
+      outputFile = `output_${outputFileIndex}.wav`;
+    }
+
+    const cmd = 'ffmpeg';
+    const args = [
+      '-f', 'alsa',
+      '-i', 'default',
+      '-acodec', 'pcm_s16le',
+      '-ar', '44100',
+      '-ac', '2',
+      outputFile
+    ];
+
+    recordingProcess = spawn(cmd, args);
+
+    recordingProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    recordingProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    recordingProcess.on('close', (code) => {
+      console.log(`child process exit ${code}`);
+      recordingProcess = null;
+      outputFile = 'output.wav'; 
+    });
+
+    res.send('Recording started');
+  } else {
+    res.send('Recording in progress');
+  }
+});
+
+app.get('/stop', (req, res) => {
+  if (recordingProcess) {
+    recordingProcess.kill('SIGINT');
+    recordingProcess.on('exit', () => {
+      res.send('Recording stopped');
+      recordingProcess = null;
+      outputFile = 'output.wav'; 
+    });
+  } else {
+    res.send('No recording to stop');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ R&D/wavToMp3Attrib2.js
@@ -0,0 +1,58 @@
+//sudo apt install ffmpeg
+//npm install fluent-ffmpeg
+
+
+
+const fs = require('fs');
+const path = require('path');
+const ffmpeg = require('fluent-ffmpeg');
+
+function convertWavToMp3(inputDir, outputDir) {
+  console.log('Conversion started...');
+ 
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
+  
+  //bitrate and sample rate
+
+  const bitrate = 128; 
+  const sampleRate = 44100; 
+
+  
+  
+  fs.readdir(inputDir, (err, files) => {
+    if (err) {
+      console.error('Error reading input folder:', err);
+      return;
+    }
+
+    files.forEach((file) => {
+      const inputFilePath = path.join(inputDir, file);
+      const outputFilePath = path.join(outputDir, `${path.basename(file, path.extname(file))}.mp3`);
+
+      // WAV to MP3 
+      ffmpeg(inputFilePath)
+        .audioBitrate(bitrate)
+        .audioFrequency(sampleRate)
+        .output(outputFilePath)
+        .outputOption('-metadata', `title=${path.basename(file, path.extname(file))}`) 
+        .outputOption('-metadata', 'artist=Artist name') 
+        .outputOption('-metadata', 'album=Album name') 
+        
+        .on('end', () => {
+          console.log(`[ ${file} ] - conversion completed `);
+          console.log(`dir location  /${outputDir} `);
+        })
+        .on('error', (err) => {
+          console.error(`Error converting ${outputFilePath}:`, err);
+        })
+        .run();
+    });
+  });
+}
+
+const inputDir = 'outputwav';
+const outputDir = 'mp3OutDir';
+convertWavToMp3(inputDir, outputDir);
+
